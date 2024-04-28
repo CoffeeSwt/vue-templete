bed041097df078707dfc75637a37aefbb8e8d2ee
Coffeesw <897884964@qq.com>
2024-04-28 14:45:34 +0800

    🎉 init: base

---
+++ README.md
@@ -0,0 +1,7 @@
+# Vue3Templete
+
+include:
+- Vue3+Vite+TypeScript
+- pinia
+- vue-router
+- axios
---
+++ vite.config.ts
@@ -0,0 +1,27 @@
+import { defineConfig, loadEnv } from 'vite'
+import * as path from 'path'
+import vue from '@vitejs/plugin-vue'
+
+export default (mode: string) => {
+  const env = loadEnv(mode, process.cwd())
+  return defineConfig({
+    plugins: [vue()],
+    resolve: {
+      //设置别名
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
+    },
+    server: {
+      proxy: {
+        [env.VITE_BASE_API]: {
+          target: `${env.VITE_BASE_URL}:${env.VITE_SERVER_PORT}`,
+          changeOrigin: true,
+          rewrite: path => path.replace(new RegExp('^' + env.VITE_BASE_API), ''),
+        },
+      }
+    }
+  })
+};
+
+
